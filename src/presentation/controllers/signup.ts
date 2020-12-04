2b9747510311756311930666ace23527c4183821
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-12-03 21:03:03 -0300

    feat: Importing InvalidParamError

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -18,7 +18,7 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    const isValid = this.emailValidator.isValid('httpRequest.body.email')
     if (!isValid) {
       return badRequest(new InvalidParamError('email'))
     }
